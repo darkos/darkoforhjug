b83a3eb345abecd6ed1c679210768b32fa26d374
darko <stefanovic.darko@gmail.com>
2016-06-23 00:16:14 -0500

    Player and card sample

--- src/main/java/darko/hjug/flashcards/Player.java
+++ src/main/java/darko/hjug/flashcards/Player.java
@@ -1,8 +1,7 @@
 package darko.hjug.flashcards;
 
-/**
- * Created by darko on 6/21/16.
- */
+import java.util.List;
+
 public class Player {
 
     public Player() {
@@ -16,5 +15,45 @@ public class Player {
     // slower, faster
     // set time
     // slideshow
-    
+    // studying mode: show question on >, show answer on next >
+    public void printSlide(String title, List<String> lines) {
+
+    }
+
+    public void printEmptyBox(int width, int height, char symbol) {
+        StringBuffer buffer = new StringBuffer();
+        int bodyLinesCount = height - 2;
+        fillFullLine(buffer, width, symbol);
+        fillEmptyLines(buffer, width, symbol, bodyLinesCount);
+        fillFullLine(buffer, width, symbol);
+        System.out.println(buffer.toString());
+    }
+
+    private void fillEmptyLines(StringBuffer buffer, int width, char symbol, int linesCount) {
+        for (int i = 0; i < linesCount; i++) {
+            fillEmptyBodyLine(buffer, width, symbol);
+        }
+    }
+
+    private void fillFullLine(StringBuffer buffer, int width, char symbol) {
+        for(int i=0; i<width; i++) {
+            buffer.append(symbol);
+        }
+        buffer.append("\n");
+    }
+
+    private void fillEmptyBodyLine(StringBuffer buffer, int width, char symbol) {
+        buffer.append(symbol);
+        for (int i = 0; i < width-2 ; i++) {
+            buffer.append(' ');
+        }
+        buffer.append(symbol);
+        buffer.append("\n");
+    }
+
+    public static void main(String[] args) {
+        Player player = new Player();
+        player.printEmptyBox(64, 15, '#');
+    }
+
 }
